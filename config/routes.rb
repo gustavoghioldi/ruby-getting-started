b262fe3327b0c43a807d3265a761755321ec10e3
gustavoghioldi <gustavoghioldi@gmail.com>
2016-02-08 03:33:33 -0300

    cambio del layout del sign

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  
   get 'productos/index'
+  
 
   resources :widgets
   resources :productos
